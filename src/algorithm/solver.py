ec5d1cb5dc2b5b31ef1c59c179e6fc21ba2e0522
Israel Adewuyi <isistickz@gmail.com>
2023-10-08 08:13:31 +0100

    feat: :construction: completed simplex algorithm.

--- src/algorithm/solver.py
+++ src/algorithm/solver.py
@@ -5,36 +5,38 @@ from ast_parser.token import TokenKind
 def advanced_simplex(A, b, C):
     n, m = A.shape
 
-    B = np.zeros((n, m - n))
+    B = np.zeros((n, n))
     C_B = np.zeros((1, n))
 
-    print(n, m, "\n")
-
-    print("I got here")
-
     for i in range(n):
-        for j in range(m - n):
+        for j in range(n):
             B[i, j] = A[i, j]
 
     for i in range(n):
         C_B[0, i] = C[0, i]
 
-    B_inverse = np.linalg.inv(B)
-    X_B = np.matmul(B_inverse, b)
-    P_table = np.round(np.matmul(B_inverse, A), 3)
+    while True:
+        B_inverse = np.linalg.inv(B)
+        X_B = np.matmul(B_inverse, b)
+        P_table = np.round(np.matmul(B_inverse, A), 3)
+        objective_values = np.matmul(C_B, P_table) - C
+        solution = np.round(np.matmul(C_B, X_B), 2)
 
-    objective_values = np.matmul(C_B, P_table) - C
+        if np.all(objective_values >= 0):
+            return X_B, solution
 
-    solution = np.round(np.matmul(C_B, X_B), 2)
+        entering_var_idx = np.argmin(objective_values)
 
+        ratios = []
+        for i in range(n):
+            if P_table[i, entering_var_idx] > 0:
+                ratios.append(X_B[i, 0] / P_table[i, entering_var_idx])
+            else:
+                ratios.append(np.inf)  
 
-    print(B, "\n")
-    print(C_B, '\n')
-    print(B_inverse, '\n')
-    print(X_B, "\n")
-    print(P_table, '\n')
-    print(objective_values, '\n')
-    print(solution)
+        exiting_var_idx = np.argmin(ratios)
+
+        B[:, exiting_var_idx] = A[:, entering_var_idx]
 
 
 def convert_to_numpy_matrices(problem):
@@ -43,10 +45,8 @@ def convert_to_numpy_matrices(problem):
 
     A = np.zeros((num_constraints, num_variables))
     C = np.zeros((1, num_variables))
-
     B = np.zeros((num_constraints, 1))
 
-    
     for i, constraint in enumerate(problem.constraints):
         for variable_name, coefficient in constraint.variables.items():
             j = list(problem.function.keys()).index(variable_name)
@@ -62,18 +62,31 @@ def convert_to_numpy_matrices(problem):
 
 def Solver():
     problem_kind = ProblemKind.MAX
-    objective_function = {"x_1": 1, "x_2": 4, "x_3": 7, "x_4":5}
+    objective_function = {"x_1": 5, "x_2": 4}
     constraints = [
-        Constraint(kind=TokenKind.EQ, variables={"x_1": 2, "x_2": 1, "x_3": 2, "x_4": 4}, solution=10),
-        Constraint(kind=TokenKind.EQ, variables={"x_1": 3, "x_2": -1, "x_3": -2, "x_4": 6}, solution=5),
+        Constraint(kind=TokenKind.LEQ, variables={"x_1": 6, "x_2": 4}, solution=24),
+        Constraint(kind=TokenKind.LEQ, variables={"x_1": 1, "x_2": 2}, solution=6),
+        Constraint(kind=TokenKind.LEQ, variables={"x_1": -1, "x_2": 1}, solution=1),
+        Constraint(kind=TokenKind.LEQ, variables={"x_1": 0, "x_2": 1}, solution=2)
     ]
+
+    for i, constraint in enumerate(constraints):
+        if constraint.kind == TokenKind.LEQ:
+            constraint.variables["s_" + str(i)] = 1.0
+            objective_function["s_" + str(i)] = 0
+
+    constraints = list(constraints)
 
     problem = Problem(problem_kind, objective_function, constraints)
 
     A, b, C = convert_to_numpy_matrices(problem)
 
-    print(A)
-    print("This is b:", b)
-    print("This is C:", C)
 
-    advanced_simplex(A, b, C)
+    # print(A)
+    # print("This is b:", b)
+    # print("This is C:", C)
+
+    objective_values, solution = advanced_simplex(A, b, C)
+
+    print("The vector of decision variables is : " , objective_values, '\n')
+    print("The optimal solution is ", solution , '\n')
