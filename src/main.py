e097572e82454edc5c2ce71f7a39956c153c7833
Israel Adewuyi <isistickz@gmail.com>
2023-10-08 08:48:35 +0100

    feat: :construction: Update to main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,29 @@
 from ast_parser.parser import Parser
 
+# parser = Parser(
+#     """                            5 x_1
+
+# + 3x_2 - 4 * _y3  2          - 8 XXXX_7  - 4 * _y3  2
+
+#       ≤ 19 * 5, 5 <= - 3 * x"""
+# )
+
 parser = Parser(
-    """                            5 x_1
+    #Tatar Paint Optimization Problem constraints
+    """
+        6x_1 + 4x_2 <= 24,
 
-+ 3x_2 - 4 * _y3  2          - 8 XXXX_7  - 4 * _y3  2
+        x_1 + 2x_2 <= 6,
 
-      ≤ 19 * 5, 5 <= - 3 * x"""
+        x_1 + x_2 <= 1,
+
+        x_2 <= 2,
+
+        Z = 5x_1 + 4x_2
+    """
 )
 
 for equation in parser.parse():
     print(equation, "\n")
+
+# solver.Solver()
